d6bceb5a740710c1cdb93397d77e14745abed3b7
bb <biabeniamin@gmail.com>
2018-11-06 19:50:43 +0200

    notification push added

--- app/src/main/java/com/example/biabe/backendgeneratorandroidsample/NotificationChecker.java
+++ app/src/main/java/com/example/biabe/backendgeneratorandroidsample/NotificationChecker.java
@@ -10,12 +10,47 @@ import android.support.v4.app.NotificationCompat;
 
 public class NotificationChecker extends IntentService {
 
+    private void sendNotification(String msg) {
 
+        NotificationManager mNotificationManager = (NotificationManager)
+
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+
+                new Intent(this, MainActivity.class), 0);
+
+
+
+        NotificationCompat.Builder mBuilder =
+
+                new NotificationCompat.Builder(this)
+
+                        .setSmallIcon(R.drawable.if_heart_1055045)
+
+                        .setContentTitle("PubNub GCM Notification")
+
+                        .setStyle(new NotificationCompat.BigTextStyle()
+
+                                .bigText(msg))
+
+                        .setContentText(msg);
+
+
+
+        mBuilder.setContentIntent(contentIntent);
+
+        mNotificationManager.notify(1, mBuilder.build());
+
+    }
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         while (true)
         {
             try {
+                sendNotification("dsfgds");
                 wait(3000);
             }
             catch (InterruptedException e)
